41b630cd291a57f8a05ddd3b2bdceef93c224c6c
Dirk G.-Meyer <65828797+DIRKGM@users.noreply.github.com>
2022-07-14 10:50:05 +0200

    Date Update

---
+++ js/date.js
@@ -0,0 +1,10 @@
+ // Date Automatic Update
+
+    date = new Date();
+    year = date.getFullYear();
+    month = date.getMonth() + 1;
+    day = date.getDate();
+    document.getElementById("Date_Update").innerHTML = "Copyright &copy; Dirk Meyer " + year;
+
+
+
